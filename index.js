97fab3cfc1b2278ba15bda86db7777f7f346a9a4
rm7054613349 <wwwritesh72660@gmail.com>
2024-10-21 09:21:34 +0530

    #

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.use(bodyParser.json());
 app.use('/api/employees', EmployeeRoutes);
 
 
+app.get("/", (req, res) => {
+	return res.json({
+		success:true,
+		message:'Your server is up and running....'
+	});
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
 })
